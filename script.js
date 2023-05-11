d8055a2d9ed4cf45219439b97b5eeb2cb3c92cfc
Jayanth <jay.codmik.18@gmail.com>
2023-05-11 21:23:33 +0530

    CV Download Fixing

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-console.log("hello Guys")
-let workWithUs = document.getElementById('WWU-btn')
-workWithUs.onclick=()=>{
-    alert('Than You For Your Response . We Will Reach back you !!')
+
+// let workWithUs = document.getElementById('WWU-btn')
+// workWithUs.onclick=()=>{
+//     alert('Than You For Your Response . We Will Reach back you !!')
+// }
+
+
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+
+
+const sendEmail=()=>{
+
+
+
+Email.send({
+    Host : "smtp.gmail.com",
+    Username : "jayanths2004@gmail.com",
+    Password : "9916337836",
+    To : 'jayanths2004@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert("sucessfull")
+);
 }
