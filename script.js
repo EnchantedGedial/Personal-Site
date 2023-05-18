1d194e24e89169a9fa6cdc1ba784c224cabe6a21
Jayanth <jay.codmik.18@gmail.com>
2023-05-18 21:10:41 +0530

    Blog Detail Page Added

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 
-// let workWithUs = document.getElementById('WWU-btn')
-// workWithUs.onclick=()=>{
-//     alert('Than You For Your Response . We Will Reach back you !!')
-// }
-
-
-<script src="https://smtpjs.com/v3/smtp.js"></script>
-
-
-const sendEmail=()=>{
 
 
 
-Email.send({
-    Host : "smtp.gmail.com",
-    Username : "jayanths2004@gmail.com",
-    Password : "9916337836",
-    To : 'jayanths2004@gmail.com',
-    From : document.getElementById("email").value,
-    Subject : "This is the subject",
-    Body : "And this is the body"
-}).then(
-  message => alert("sucessfull")
-);
-}
+
